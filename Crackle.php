8d8abbb3e1f514df60aa2310407684c2ead92eca
George Brighton <george@gebn.co.uk>
2013-12-13 11:26:30 +0000

    Improved initialisation process.

--- Crackle.php
+++ Crackle.php
@@ -2,16 +2,18 @@
 
 namespace Crackle {
 
+	use \Exception;
+
 	/*
 	 * This file is self-running - it just needs to be `include`d on the page before Crackle is used.
 	 */
-	new Crackle();
+	Crackle::load();
 
 	/**
 	 * Sets up the environment required by Crackle.
 	 * @author George Brighton
 	 */
-	class Crackle {
+	final class Crackle {
 
 		/**
 		 * The version of Crackle in use.
@@ -20,16 +22,49 @@ namespace Crackle {
 		const VERSION = '1.0';
 
 		/**
+		 * This class should not be instantiated.
+		 */
+		private function __construct() {}
+
+		/**
+		 * Readies Crackle for use.
+		 */
+		public static function load() {
+			static $loaded = false;
+
+			// we only want to initialise once
+			if(!$loaded) {
+				self::initialise();
+				$loaded = true;
+			}
+		}
+
+		/**
 		 * Carries out everything needed to get Crackle up and running.
 		 */
-		public function __construct() {
-			$this->registerAutoloader();
+		private static function initialise() {
+			self::checkRequirements();
+			self::registerAutoloader();
+		}
+
+		/**
+		 * Ensures this installation of PHP meets Crackle's minimum requirements.
+		 * @throws \Exception		If any requirement is not met.
+		 */
+		private static function checkRequirements() {
+			if(version_compare(PHP_VERSION, '5.3.0', '<')) {
+				throw new Exception('Crackle requires PHP 5.3.0 or later.');
+			}
+
+			if(!extension_loaded('curl')) {
+				throw new Exception('The cURL module must be available for Crackle to operate.');
+			}
 		}
 
 		/**
 		 * Adds the autoloader function to the queue.
 		 */
-		private function registerAutoloader() {
+		private static function registerAutoloader() {
 			spl_autoload_register(function($qualified) {
 				require substr($qualified, 8) . '.php';
 			}, true);
