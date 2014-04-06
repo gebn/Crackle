abc50d99e43cef889ced1bddd99269cb73b936c7
George Brighton <george@gebn.co.uk>
2014-04-07 00:14:40 +0100

    Implemented a more liberal version of parse_url().

--- Requests/Fields/Parameters.php
+++ Requests/Fields/Parameters.php
@@ -37,11 +37,7 @@ namespace Crackle\Requests\Fields {
 
 			if(isset($parts['query'])) {
 				// extract GET parameters and add them as fields
-				$params = array();
-				parse_str($parts['query'], $params);
-				foreach($params as $name => $value) {
-					$this->add($name, $value);
-				}
+				$this->import($parts['query']);
 			}
 
 			// if the URL contains a host with no trailing slash, this key will not exist
@@ -51,6 +47,25 @@ namespace Crackle\Requests\Fields {
 
 			// return the original URL without the parameters
 			return sprintf('%s://%s%s', $parts['scheme'], $parts['host'], $parts['path']);
+		}
+
+		/**
+		 * Parses a query string, importing all parameters. Similar to parse_url() without its quirks.
+		 * @param string $string					The query string to parse and import.
+		 * @throws InvalidArgumentException			If the query string is very malformed.
+		 */
+		private final function import($string) {
+			foreach(explode('&', $string) as $part) {
+				$equals = strpos($part, '=');
+
+				if($equals === false) { // minimal validation - we don't check there's only 1 '=' etc.
+					throw new InvalidArgumentException('Malformed query string.');
+				}
+
+				$this->add(
+						urldecode(substr($part, 0, $equals)), // name
+						urldecode(substr($part, $equals + 1))); // value
+			}
 		}
 	}
 }
