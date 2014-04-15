4c9ff21303719e67a5c132a9ddbbeaa05ca272ce
George Brighton <george@gebn.co.uk>
2014-04-15 03:38:43 +0100

    Updated version to 2.2.

--- Crackle.php
+++ Crackle.php
@@ -24,7 +24,7 @@ namespace Crackle {
 		 * The version of Crackle in use.
 		 * @var string
 		 */
-		const VERSION = '2.1';
+		const VERSION = '2.2';
 
 		/**
 		 * This class should not be instantiated.
